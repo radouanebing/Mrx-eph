064598590e1eaf4b3af0425de6df59061931312e
loudjaina1985 <naghnaghradiuane@gmail.com>
2026-06-02 03:33:58 +0100

    Final check for Firebase connection

--- src/components/EmployeeManager.tsx
+++ src/components/EmployeeManager.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Users, UserPlus, Trash2 } from 'lucide-react';
-import { db } from './firebaseConfig';
-import { collection, onSnapshot, addDoc, deleteDoc, doc } from 'firebase/firestore';
+import { db } from './firebaseConfig'; 
+import { collection, onSnapshot } from 'firebase/firestore';
 import { Employee } from '../types';
 
 export const EmployeeManager: React.FC = () => {
